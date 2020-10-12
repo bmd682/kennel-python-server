3acad6dc85c8f2211846b840d12551d54b8760b9
Brett Derrington <brett.derrington@gmail.com>
2020-10-12 10:56:11 -0500

    Post and Create for animals

--- request_handler.py
+++ request_handler.py
@@ -1,5 +1,7 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
 from animals import get_all_animals, get_single_animal
+from animals import create_animal
 from employees import get_single_employee, get_all_employees
 from locations import get_single_location, get_all_locations
 
@@ -66,13 +68,27 @@ class HandleRequests(BaseHTTPRequestHandler):
     # Here's a method on the class that overrides the parent's method.
     # It handles any POST request.
     def do_POST(self):
-        # Set response code to 'Created'
         self._set_headers(201)
-
         content_len = int(self.headers.get('content-length', 0))
         post_body = self.rfile.read(content_len)
-        response = f"received post request:<br>{post_body}"
-        self.wfile.write(response.encode())
+
+        # Convert JSON string to a Python dictionary
+        post_body = json.loads(post_body)
+
+        # Parse the URL
+        (resource, id) = self.parse_url(self.path)
+
+        # Initialize new animal
+        new_animal = None
+
+        # Add a new animal to the list. Don't worry about
+        # the orange squiggle, you'll define the create_animal
+        # function next.
+        if resource == "animals":
+            new_animal = create_animal(post_body)
+
+        # Encode the new animal and send in response
+        self.wfile.write(f"{new_animal}".encode())
 
 
     # Here's a method on the class that overrides the parent's method.
